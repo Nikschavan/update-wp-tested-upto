af30694e0aeea6e52e3aba28b46414904eb4e79c
Nikhil Chavan <email@nikhilchavan.com>
2019-09-30 12:20:28 +0530

    Change tested up to in the readme

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,10 @@
 # doesn't match I want to be able to show an error first
 set -eo
 
-jq --version
-
 API_URL=https://api.wordpress.org/core/version-check/1.7/
 LATEST_WP=$(curl $API_URL | jq ".offers[0].version")
 
 echo $LATEST_WP
-
-# sh -c "git config --global user.email 'update-tested-upto-wp@bsf.io' \
-# 	  && git config --global user.name 'Update Tested upto Bot on GitHub' \
-# 	  &&git checkout -b 'update-tested-upto-$LATEST_WP' \
-#       && git add -A && git commit -m 'Updated WordPress tested upto to latest WP version by bsf-bot' --allow-empty \
-#       && git push -u origin update-tested-upto-$LATEST_WP"
+STABLE_TAG=$(grep -m 1 "^Tested up to:" "$GITHUB_WORKSPACE/readme.txt" | tr -d '\r\n' | awk -F ' ' '{print $NF}')
+echo $STABLE_TAG
+sed -i "s/Tested up to: $STABLE_TAG/$LATEST_WP/" "$GITHUB_WORKSPACE"/readme.txt
