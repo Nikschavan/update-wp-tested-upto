7ae3319fd03f04cbd5f504ebb862cea72ec66501
Nikhil Chavan <email@nikhilchavan.com>
2019-09-30 11:46:43 +0530

    Check version of jq installed

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,15 @@
 # doesn't match I want to be able to show an error first
 set -eo
 
+jq --version
+
 API_URL=https://api.wordpress.org/core/version-check/1.7/
 LATEST_WP=$(curl $API_URL | jq ".offers[0].version")
 
 echo $LATEST_WP
 
-sh -c "git config --global user.email 'update-tested-upto-wp@bsf.io' \
-	  && git config --global user.name 'Update Tested upto Bot on GitHub' \
-	  &&git checkout -b 'update-tested-upto-$LATEST_WP' \
-      && git add -A && git commit -m 'Updated WordPress tested upto to latest WP version by bsf-bot' --allow-empty \
-      && git push -u origin update-tested-upto-$LATEST_WP"
+# sh -c "git config --global user.email 'update-tested-upto-wp@bsf.io' \
+# 	  && git config --global user.name 'Update Tested upto Bot on GitHub' \
+# 	  &&git checkout -b 'update-tested-upto-$LATEST_WP' \
+#       && git add -A && git commit -m 'Updated WordPress tested upto to latest WP version by bsf-bot' --allow-empty \
+#       && git push -u origin update-tested-upto-$LATEST_WP"
