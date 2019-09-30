5af4b98a6f61b66bd3fef2ee4769e1e9c3e3d706
Nikhil Chavan <email@nikhilchavan.com>
2019-09-30 12:28:46 +0530

    commit the changeset

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,18 @@ set -eo
 API_URL=https://api.wordpress.org/core/version-check/1.7/
 LATEST_WP=$(curl $API_URL | jq ".offers[0].version")
 
+cd $GITHUB_WORKSPACE
+
+git config --global user.email 'update-tested-upto-wp@bsf.io'
+git config --global user.name 'Update Tested upto Bot on GitHub'
+
 echo $LATEST_WP
 STABLE_TAG=$(grep -m 1 "^Tested up to:" "$GITHUB_WORKSPACE/readme.txt" | tr -d '\r\n' | awk -F ' ' '{print $NF}')
 echo $STABLE_TAG
+
+git checkout -b 'update-tested-upto-$LATEST_WP'
+
 sed -i "s/Tested up to: $STABLE_TAG/$LATEST_WP/" "$GITHUB_WORKSPACE"/readme.txt
+
+sh -c "git add -A && git commit -m 'Updated WordPress tested upto to latest WP version by bsf-bot' --allow-empty \
+      && git push -u origin update-tested-upto-$LATEST_WP"
