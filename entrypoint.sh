3f6599468f5ed5cb4444f1bfd04debf47bc37d3b
Nikhil Chavan <email@nikhilchavan.com>
2019-09-30 12:56:11 +0530

    don't try to commit the changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,5 +20,5 @@ git checkout -b "update-tested-upto-$LATEST_WP"
 
 sed -i "s/Tested up to: $STABLE_TAG/$LATEST_WP/" "$GITHUB_WORKSPACE"/readme.txt
 
-sh -c "git add -A && git commit -m 'Updated WordPress tested upto to latest WP version by bsf-bot' --allow-empty \
-      && git push -u origin update-tested-upto-$LATEST_WP"
+# sh -c "git add -A && git commit -m 'Updated WordPress tested upto to latest WP version by bsf-bot' --allow-empty \
+#       && git push -u origin update-tested-upto-$LATEST_WP"
